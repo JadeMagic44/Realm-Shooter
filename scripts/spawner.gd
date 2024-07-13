a4be8cea8c32ee9fdea87758aeafa4a44e32071c
StealthDrifter <53316578+StealthDrifter@users.noreply.github.com>
2024-07-13 16:10:06 -0700

    Add files via upload

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -1,21 +1,18 @@
 extends Node2D
 
 var loadEnemy = preload("res://scenes/enemy.tscn")
-var timer = 0
 var spawnTimer = 2
-@onready var count_timer = $countTimer
+@onready var spawn_timer = $spawnTimer
 
 func _on_spawn_timer_timeout():
 	var enemy = loadEnemy.instantiate()
 	enemy.position = Vector2(randi_range(200, 700), randi_range(200, 700))
 	add_child(enemy)
-	count_timer.set_wait_time(spawnTimer)
-	print(timer)
 	print(spawnTimer)
 
 
 
 func _on_count_timer_timeout():
-	timer += 1
-	if timer % 30 == 0:
-		spawnTimer -= 0.05
+	if spawnTimer > 0.5:
+		spawnTimer -= 0.5
+	spawn_timer.set_wait_time(spawnTimer)
