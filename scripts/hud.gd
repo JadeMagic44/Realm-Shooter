54452528fa435b52988025155d3ac08fc7f02349
StealthDrifter <53316578+StealthDrifter@users.noreply.github.com>
2024-07-13 20:13:15 -0700

    Add files via upload

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -6,3 +6,19 @@ var min = 0
 
 func _process(delta):
 	$score.text = "score:" + str(global.score)
+	if min == 0:
+		if sec < 10:
+			$time.text = "0:0" + str(sec)
+		else:
+			$time.text = "0:" + str(sec)
+	elif sec < 10:
+		$time.text = str(min) + ":0" + str(sec)
+	else:
+		$time.text = str(min) + ":" + str(sec)
+
+
+func countup():
+	sec += 1
+	if sec >= 60:
+		min += 1
+		sec = 0
